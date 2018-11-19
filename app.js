38d1a47ccacf9aab0fd4415ef595aba5aad24c9f
Ritik Srivastava <ritik.space@gmail.com>
2018-11-19 17:02:10 +0000

    New User

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose");
 var fs = require("fs");
 var multer = require('multer');
 
-mongoose.connect("mongodb://localhost/users");
+mongoose.connect("mongodb://localhost/sanrakshak", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(BodyParser.urlencoded({ extended: true }))
 
@@ -23,18 +23,42 @@ var UserSchema = new mongoose.Schema({
     gender: String,
     dob: String
 });
-var User = mongoose.model("User", UserSchema);
+var User = mongoose.model("users", UserSchema);
 
 app.get("/check", function(req, res) {
     var email = req.query.email;
-    User.find({ email: email }, function(err, user) {
-        if (err) { console.log("Error occured while checking for email.\n" + err); }
+    User.find({ email: email }, function(e, user) {
+        if (e) { console.log("Error occured while checking for email.\n" + e); }
         else {
             if (user.length) { res.send("1"); }
             else { res.send("0"); }
         }
     });
 });
+app.get("/signup", function(req, res) {
+    var email = req.query.email;
+    var pass = req.query.pass;
+    User.create({
+        email: email,
+        pass: pass,
+        fname: "",
+        lname: "",
+        gender: "",
+        dob: ""
+    }, function(e, user) {
+        if (e) {
+            res.send("0");
+            console.log("Error occured while creating user.\n" + e);
+        }
+        else {
+            res.send("1");
+            console.log("User created : ");
+            console.log(user);
+        }
+    });
+});
+
+
 app.get("*", function(req, res) {
     res.send("Working!!!");
 });
