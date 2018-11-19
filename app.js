d7c1ece429248774cda42f7e8f22ecf732bab852
EC2 Default User <ec2-user@ip-172-31-27-108.us-east-2.compute.internal>
2018-11-19 16:30:03 +0000

    Login cont.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var mongoose = require("mongoose");
 var fs = require("fs");
 var multer = require('multer');
 
+mongoose.connect("mongodb://localhost/users");
 app.set("view engine", "ejs");
 app.use(BodyParser.urlencoded({ extended: true }))
 
@@ -27,14 +28,15 @@ var User = mongoose.model("User", UserSchema);
 app.get("/check", function(req, res) {
     var email = req.query.email;
     User.find({ email: email }, function(err, user) {
-        if (err) {
-            console.log("Error occured while checking for email.\n" + err);
-        }
+        if (err) { console.log("Error occured while checking for email.\n" + err); }
         else {
             if (user.length) { res.send("1"); }
             else { res.send("0"); }
         }
     });
+});
+app.get("*", function(req, res) {
+    res.send("Working!!!");
 });
 
 app.listen(8080, function() {
