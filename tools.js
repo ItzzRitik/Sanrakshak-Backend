d8cdf892cc0da0ae1a58043344205bfdf96d6247
Ritik Srivastava <ritik.space@gmail.com>
2018-11-21 12:08:44 +0000

    Send Verification URL

--- tools.js
+++ tools.js
@@ -1,32 +1,4 @@
 module.exports = {
-  sendMail: function(nodemailer, senderemail, senderpass, email, res, user) {
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: senderemail,
-        pass: senderpass
-      }
-    });
-    const mailOptions = {
-      from: senderemail,
-      to: email,
-      subject: "Email Verification",
-      html: "<p>Your html here</p>"
-    };
-    transporter.sendMail(mailOptions, function(e, info) {
-      if (e) {
-        res.send("0");
-        console.log(">  Couldn't Send Verification Email\n   >  " + e.message.substring(0, 13));
-        user.deleteMany({ email: email }, function(e, obj) {
-          if (e) throw e;
-        });
-      }
-      else {
-        res.send("1");
-        console.log(">  Verification Email Sent");
-      }
-    });
-  },
   sendVerificationMail: function(mailgun, email, message, res, user) {
     var data = {
       from: "Sanrakshak <verify@sanrakshak.in>",
@@ -37,8 +9,8 @@ module.exports = {
     mailgun.messages().send(data, function(e, body) {
       if (e) {
         res.send("0");
-        console.log(">  Couldn't Send Verification Email\n   >  " + e.message.substring(0, 13));
-        user.deleteMany({ email: email }, function(e, obj) {
+        console.log(">  Couldn't Send Verification Email\n   >  " + e.message);
+        user.remove({ email: email }, function(e, obj) {
           if (e) throw e;
         });
       }
